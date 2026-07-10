2cfb8d49c136fc24e30cd6618bea0fb82ff21c1f
Kill3rEz <98993017+Kill3rEz@users.noreply.github.com>
2026-07-10 12:43:59 +0200

    dodge: Extend LinearmotorVibratorService for Oplus camera vibration
    Update vibrate() to take uid, opPkg and an IBinder token, and add
    cancelVibrate, vibrator status, strength/style settings accessors and
    a Richtap blacklist check needed to support vibration on the Oplus
    camera.
    
    Change-Id: I6e6ec4e207250c2dbc8f3230b5e3ce1bb49315e2

--- services/core/java/com/android/server/vibrator/LinearmotorVibratorService.java
+++ services/core/java/com/android/server/vibrator/LinearmotorVibratorService.java
@@ -7,6 +7,7 @@ package com.android.server.vibrator;
 
 import android.content.Context;
 import android.os.Binder;
+import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Slog;
@@ -15,6 +16,7 @@ import com.android.server.SystemService;
 
 import com.oplus.os.ILinearmotorVibratorService;
 import com.oplus.os.WaveformEffect;
+
 
 public class LinearmotorVibratorService extends SystemService {
 
@@ -29,7 +31,7 @@ public class LinearmotorVibratorService extends SystemService {
 
     private final ILinearmotorVibratorService.Stub mService = new ILinearmotorVibratorService.Stub() {
         @Override
-        public void vibrate(WaveformEffect effect) {
+        public void vibrate(int uid, String opPkg, WaveformEffect effect, IBinder token) {
             synchronized (mLock) {
                 final long ident = Binder.clearCallingIdentity();
                 try {
@@ -40,6 +42,58 @@ public class LinearmotorVibratorService extends SystemService {
                     Binder.restoreCallingIdentity(ident);
                 }
             }
+        }
+
+        @Override
+        public void cancelVibrate(WaveformEffect effect, IBinder token) {
+            synchronized (mLock) {
+                mVibrator.cancel();
+            }
+        }
+
+        @Override
+        public int getVibratorStatus() {
+            return 0; // always ready
+        }
+
+        @Override
+        public void setVibratorStrength(int strength) {
+            // no-op
+        }
+
+        @Override
+        public int getSettingsTouchEffectStrength() {
+            return 2400;
+        }
+
+        @Override
+        public int getSettingsRingtoneEffectStrength() {
+            return 2400;
+        }
+
+        @Override
+        public int getSettingsNotificationEffectStrength() {
+            return 2400;
+        }
+
+        @Override
+        public int getVibratorTouchStyle() {
+            return 0;
+        }
+
+        @Override
+        public void setVibratorTouchStyle(int style) {
+            // no-op
+        }
+
+        @Override
+        public void updateVibrationAmplitude(int uid, String opPkg, float amplitudeRatio) {
+            // no-op
+        }
+
+        @Override
+        public boolean checkRichtapBlackList(String packageName) {
+            return false;
         }
     };
 
